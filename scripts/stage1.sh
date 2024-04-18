e44af67e7464a8f733e516e973a2b1137261a1c2
XaPoHbomj <sashavaschenco@mail.ru>
2024-04-19 02:59:52 +0300

    Fix typo

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -4,7 +4,7 @@ rm data/*.csv
 unzip data/dataset.zip -d data/
 
 # Run python script to create tables, import data and test database
-python3 scripts/stage1-scripts/build_projectdb.py
+python3 scripts/stage1_scripts/build_projectdb.py
 
 # Import password as variable
 password=$(head -n 1 secrets/.psql.pass)
